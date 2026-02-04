51fe367d3718014227c6e47906c0a586c2af3205
Maurine Denoual <maurine.denoual2025@campus-eni.fr>
2026-02-05 00:12:19 +0100

    mise en place des filtres pour le formulaire

--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Participant;
 use App\Entity\Sortie;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -15,7 +16,82 @@ class SortieRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Sortie::class);
     }
+    /**
+     * Récupère les sorties en fonction des filtres de recherche et des règles de visibilité
+     */
+    public function findSearch(Participant $utilisateur, array $donnees): array
+    {
+        // On crée le QueryBuilder de base sur l'entité Sortie (alias 's')
+        $qb = $this->createQueryBuilder('s')
+            ->leftJoin('s.etat', 'e') // Jointure pour filtrer sur le libellé de l'état
+            ->addSelect('e')
+            ->leftJoin('s.campus', 'c') // Jointure pour le filtre par campus
+            ->addSelect('c');
 
+        // --- RÈGLE DE VISIBILITÉ ---
+        // On ne veut voir que les sorties publiées OU celles dont on est l'organisateur
+        $qb->andWhere(
+            $qb->expr()->orX(
+                'e.libelle != :etatCreer',
+                's.organisateur = :user'
+            )
+        )
+            ->setParameter('etatCreer', 'En création')
+            ->setParameter('user', $utilisateur);
+
+        // --- FILTRES DYNAMIQUES (FORMULAIRE) ---
+
+        // 1. Filtre par Campus
+        if (!empty($donnees['campus'])) {
+            $qb->andWhere('s.campus = :campus')
+                ->setParameter('campus', $donnees['campus']);
+        }
+
+        // 2. Filtre par Nom (recherche partielle)
+        if (!empty($donnees['nom'])) {
+            $qb->andWhere('s.nom LIKE :nom')
+                ->setParameter('nom', '%' . $donnees['nom'] . '%');
+        }
+
+        // 3. Filtre par Dates (Intervalle)
+        if (!empty($donnees['dateDebut'])) {
+            $qb->andWhere('s.dateHeureDebut >= :dateDebut')
+                ->setParameter('dateDebut', $donnees['dateDebut']);
+        }
+        if (!empty($donnees['dateFin'])) {
+            $qb->andWhere('s.dateHeureDebut <= :dateFin')
+                ->setParameter('dateFin', $donnees['dateFin']);
+        }
+
+        // 4. Filtre "Sorties dont je suis l'organisateur"
+        if (!empty($donnees['isOrganisateur'])) {
+            $qb->andWhere('s.organisateur = :user');
+            // Le paramètre :user est déjà défini plus haut
+        }
+
+        // 5. Filtre "Sorties auxquelles je suis inscrit"
+        if (!empty($donnees['isInscrit'])) {
+            $qb->andWhere(':user MEMBER OF s.inscrits');
+        }
+
+        // 6. Filtre "Sorties auxquelles je ne suis PAS inscrit"
+        if (!empty($donnees['isNotInscrit'])) {
+            $qb->andWhere(':user NOT MEMBER OF s.inscrits');
+        }
+
+        // 7. Filtre "Sorties passées"
+        if (!empty($donnees['isTerminee'])) {
+            $qb->andWhere('e.libelle = :etatPasser')
+                ->setParameter('etatPasser', 'Passée');
+        }
+
+        // On ordonne par date de début (la plus proche en premier)
+        $qb->orderBy('s.dateHeureDebut', 'ASC');
+
+        return $qb->getQuery()->getResult();
+    }
+
+}
     //    /**
     //     * @return Sortie[] Returns an array of Sortie objects
     //     */
@@ -40,4 +116,3 @@ class SortieRepository extends ServiceEntityRepository
     //            ->getOneOrNullResult()
     //        ;
     //    }
-}
