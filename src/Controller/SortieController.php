097e25e7ad05c9a375c12e300597b3b9be08da4d
Maurine Denoual <maurine.denoual2025@campus-eni.fr>
2026-02-05 22:57:47 +0100

    modification nom route accueil + création route 'créer sortie'

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,15 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
+use App\Entity\Sortie;
 use App\Form\SearchSortieType;
+use App\Form\SortieType;
+use App\Repository\EtatRepository;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 class SortieController extends AbstractController
 {
-    #[Route('/accueil', name: 'sortie_liste')]
+    // --- CREATION DE LA ROUTE ACCUEIL --- //
+    #[Route('/accueil', name: 'app_accueil')]
     public function liste(SortieRepository $sortieRepository, Request $request): Response
     {
         // 1. Récupération de l'utilisateur connecté (le Participant)
@@ -36,6 +41,53 @@ class SortieController extends AbstractController
         return $this->render('sortie/accueil.html.twig', [
             'form' => $form->createView(),
             'sorties' => $sorties,
+        ]);
+    }
+
+    // --- CREATION DE LA ROUTE "Créer une sortie" --- //
+    #[Route('/sortie/creer', name: 'app_sortie_creer')]
+    public function creer(Request $request, EntityManagerInterface $entityManager, EtatRepository $etatRepository): Response
+    {
+        // 1. On crée une instance vide
+        $sortie = new Sortie();
+
+        // 2. On récupère l'utilisateur connecté (organisateur)
+        /** @var Participant $user */
+        $user = $this->getUser();
+        $sortie->setOrganisateur($user);
+
+        // 3. On définit le campus de la sortie (celui de l'organisateur)
+        $sortie->setCampus($user->getCampus());
+
+        // 4 . On crée le formualire
+        $form = $this->createForm(SortieType::class, $sortie);
+        $form->handleRequest($request);
+
+        //5. Traitemnent du formulaire
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // GESTION DES BOUTONS "ENREGISTRER" vs "PUBLIER"
+            // 1. On cherche l'état correspondant dans la base de données
+            if ($request->request->has('Publier')){
+                $etat = $etatRepository->findOneBy(['libelle' => 'Ouverte']);
+            } else {
+                $etat = $etatRepository->findOneBy(['libelle' => 'En création']);
+            }
+
+            // 2. Affichage du résultat
+            $sortie->setEtat($etat);
+
+            // 3. Sauvegarde de BDD
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            // 4. Message pop-up de validation d'action et redirection de l'utilisateur
+            $this->addFlash('success', 'La Sortie a bien été ajoutée !');
+            return $this->redirectToRoute('app_accueil');
+        }
+
+        return $this->render('sortie/creer.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
