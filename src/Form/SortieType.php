fcab1b8d3874a771a48815d396e666df532ac7eb
Maurine Denoual <maurine.denoual2025@campus-eni.fr>
2026-02-05 22:57:40 +0100

    modification nom route accueil + création route 'créer sortie'

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,7 +2,16 @@
 
 namespace App\Form;
 
+use App\Entity\Lieu;
+use App\Entity\Sortie;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,14 +20,59 @@ class SortieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
-        ;
+            // Nom de l'événement
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la sortie'
+            ])
+            // Date et heure du début (widget single_text pour l'agenda HTML5)
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'label' => 'Date et heure de la sortie',
+                'widget' => 'single_text',
+                'required' => true,
+            ])
+            // Date limite pour s'inscrire
+            ->add('dateLimiteInscription', DateType::class, [
+                'label' => 'Date limite d\'inscription',
+                'widget' => 'single_text',
+                'required' => true,
+            ])
+            // Nombre de places disponibles
+            ->add('nbInscriptionsMax', IntegerType::class, [
+                'label' => 'Nombre de places',
+                'attr' => ['min' => 1]
+            ])
+            // Durée de l'activité
+            ->add('duree', IntegerType::class, [
+                'label' => 'Durée (en minutes)',
+                'attr' => ['step' => 15]
+            ])
+            // Description libre
+            ->add('infosSortie', TextareaType::class, [
+                'label' => 'Description et infos',
+                'required' => false,
+            ])
+            // Ville : Non "mappé" car la sortie est liée au Lieu, pas directement à la Ville
+            ->add('ville', EntityType::class, [
+                'class' => Ville::class,
+                'choice_label' => 'nom',
+                'placeholder' => '-- Choisir une ville --',
+                'mapped' => false,
+                'label' => 'Ville'
+            ])
+            // Lieu : Sélection du lieu précis
+            ->add('lieu', EntityType::class, [
+                'class' => Lieu::class,
+                'choice_label' => 'nom',
+                'placeholder' => '-- Choisir un lieu --',
+                'label' => 'Lieu'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            // On lie le formulaire à l'entité Sortie
+            'data_class' => Sortie::class,
         ]);
     }
 }
