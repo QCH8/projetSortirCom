39cd7e10f87dc3f29725574067258bb1d0828443
QCH8 <quentin.chetail@gmail.com>
2026-02-05 11:51:21 +0100

    ParticipationController - Inscription à un événement 🦖

--- src/Controller/ParticipationController.php
+++ src/Controller/ParticipationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use App\Entity\Sortie;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,6 +25,12 @@ final class ParticipationController extends AbstractController
         $participant = $this->getUser();
         if(!$participant){
             $this->addFlash("error", "Vous devez être connecté.");
+            return $this->redirectToRoute("app_connexion");
+        }
+
+        //bien une instance de Participant
+        if(!$participant instanceof Participant){
+            $this->addFlash("error", "Utilisateur Invalide.");
             return $this->redirectToRoute("app_connexion");
         }
 
@@ -56,10 +63,71 @@ final class ParticipationController extends AbstractController
         $manager->persist($sortie);
         $manager->flush();
 
-        $this->addFlash("success", "Désincription prise en compte.");
+        $this->addFlash("success", "Désinscription prise en compte.");
         return $this->redirectToRoute("app_sorties");
     }
 
+    #[Route('/sorties/{id}/inscription', name: 'participation_incription', requirements: ['\d+'], methods: ["POST"])]
+    public function inscriptionSortie(
+        Sortie $sortie,
+        EntityManagerInterface $manager,
+        Request $request,
+    ): RedirectResponse
+    {
+        //Accès User only
+        $this->denyAccessUnlessGranted("ROLE_USER");
 
+        $participant = $this->getUser();
+        if(!$participant){
+            $this->addFlash("error", "Vous devez être connecté.");
+            return $this->redirectToRoute("app_connexion");
+        }
+
+        //bien une instance de Participant
+        if(!$participant instanceof Participant){
+            $this->addFlash("error", "Utilisateur Invalide.");
+            return $this->redirectToRoute("app_connexion");
+        }
+
+        //Protection vs CSRF
+        //todo: verif génération du token
+        if(!$this->isCsrfTokenValid('inscrire'.$sortie->getId(), (string) $request->request->get('_token')))
+        {
+            $this->addFlash("error", "Action non autorisée (CSRF).");
+            return $this->redirectToRoute("app_sorties");
+        }
+
+        //Vérification : la date limite inscription n'est pas dépassée.
+        $now = new \DateTimeImmutable("now", "Europe/Paris");
+
+        if($sortie->getDateLimiteInscription() <= $now){
+            $this->addFlash("error", "Impossible de s'inscrire, la date limite est dépassée.");
+            //todo: changement de redirect vers la page détail de la sortie
+            return $this->redirectToRoute("app_sorties");
+        }
+
+        //Vérification : il reste des places dans la sortie
+        if($sortie->getInscrits()->count() >= $sortie->getNbInscriptionsMax()){
+            $this->addFlash("error", "Impossible de s'inscrire, limite de participants atteinte.");
+            //todo: changement de redirect vers la page détail de la sortie
+            return $this->redirectToRoute("app_sorties");
+        }
+
+
+        //Vérification : le participant n'est pas déjà inscrit
+        if($sortie->getInscrits()->contains($participant)){
+            $this->addFlash("error", "Impossible de s'inscrire, vous êtes déjà inscrit dans cette sortie.");
+            //todo: changement de redirect vers la page détail de la sortie
+            return $this->redirectToRoute("app_sorties");
+        }
+
+        //Si rien n'est déclenché : on inscrit
+        $sortie->addInscrit($participant);
+        $manager->persist($sortie);
+        $manager->flush();
+
+        $this->addFlash("success", "Inscription prise en compte.");
+        return $this->redirectToRoute("app_sorties");
+    }
 
 }
