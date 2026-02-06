358a6d44fd3b02bb86badc4ea72781c21ed037eb
QCH8 <quentin.chetail@gmail.com>
2026-02-06 17:41:09 +0100

    Màj état ouvert → clôture en cas de capacité max atteinte ou date limite dépassée🦖

--- src/Controller/ParticipationController.php
+++ src/Controller/ParticipationController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ParticipationController extends AbstractController
 {
-    #[Route('/sorties/{id}/desinscription', name: 'participation_desinscription', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    #[Route('/sortie/{id}/desinscription', name: 'participation_desinscription', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
     public function desinscriptionSortie(
         Sortie $sortie,
         EntityManagerInterface $manager,
@@ -43,7 +43,7 @@ final class ParticipationController extends AbstractController
         if(!$this->isCsrfTokenValid('desister'.$sortie->getId(), (string) $request->request->get('_token')))
         {
             $this->addFlash("error", "Action non autorisée (CSRF).");
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
         */
 
@@ -53,14 +53,14 @@ final class ParticipationController extends AbstractController
         if($sortie->getDateHeureDebut() <= $now){
             $this->addFlash("error", "Impossible de se désister, la sortie a déjà débuté.");
             //todo: changement de redirect vers la page détail de la sortie
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
 
         //Vérification : l'User est bien inscrit
         if(!in_array($participant, $sortie->getInscrits()->toArray(), true)){
             $this->addFlash("error", "Vous n'êtes pas inscrit à cette sortie.");
             //todo: changement de redirect vers la page détail de la sortie
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
 
         //Si rien n'est déclenché : on désinscrit
@@ -69,10 +69,10 @@ final class ParticipationController extends AbstractController
         $manager->flush();
 
         $this->addFlash("success", "Désinscription prise en compte.");
-        return $this->redirectToRoute("app_home");
+        return $this->redirectToRoute("app_accueil");
     }
 
-    #[Route('/sorties/{id}/inscription', name: 'participation_inscription', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    #[Route('/sortie/{id}/inscription', name: 'participation_inscription', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
     public function inscriptionSortie(
         Sortie $sortie,
         EntityManagerInterface $manager,
@@ -101,7 +101,7 @@ final class ParticipationController extends AbstractController
         if(!$this->isCsrfTokenValid('inscrire'.$sortie->getId(), (string) $request->request->get('_token')))
         {
             $this->addFlash("error", "Action non autorisée (CSRF).");
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
         */
 
@@ -111,21 +111,21 @@ final class ParticipationController extends AbstractController
         if($sortie->getDateLimiteInscription() <= $now){
             $this->addFlash("error", "Impossible de s'inscrire, la date limite est dépassée.");
             //todo: changement de redirect vers la page détail de la sortie
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
 
         //Vérification : il reste des places dans la sortie
         if($sortie->getInscrits()->count() >= $sortie->getNbInscriptionsMax()){
             $this->addFlash("error", "Impossible de s'inscrire, limite de participants atteinte.");
             //todo: changement de redirect vers la page détail de la sortie
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
 
         //Vérification : le participant n'est pas déjà inscrit
         if($sortie->getInscrits()->contains($participant)){
             $this->addFlash("error", "Impossible de s'inscrire, vous êtes déjà inscrit dans cette sortie.");
             //todo: changement de redirect vers la page détail de la sortie
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
 
         //Si rien n'est déclenché : on inscrit
@@ -134,10 +134,10 @@ final class ParticipationController extends AbstractController
         $manager->flush();
 
         $this->addFlash("success", "Inscription prise en compte.");
-        return $this->redirectToRoute("app_home");
+        return $this->redirectToRoute("app_accueil");
     }
 
-    #[Route('/sorties/{id}/publication', name: 'participation_publication', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    #[Route('/sortie/{id}/publication', name: 'participation_publication', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
     public function publicationSortie(
         Sortie $sortie,
         EtatRepository $etatRepo,
@@ -159,7 +159,7 @@ final class ParticipationController extends AbstractController
         //bien l'organisateur de l'event
         if($participant !== $sortie->getOrganisateur()){
             $this->addFlash("error", "Vous n'êtes pas l'organisateur.");
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
         //todo: décommenter une fois la mise en place du token CSRF sur les pages twig
         /*
@@ -168,7 +168,7 @@ final class ParticipationController extends AbstractController
         if(!$this->isCsrfTokenValid('inscrire'.$sortie->getId(), (string) $request->request->get('_token')))
         {
             $this->addFlash("error", "Action non autorisée (CSRF).");
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
         */
 
@@ -178,13 +178,13 @@ final class ParticipationController extends AbstractController
         if($sortie->getDateLimiteInscription() <= $now){
             $this->addFlash("error", "Pas de publication d'événements dans le passé.");
             //todo: changement de redirect vers la page détail de la sortie
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
         //Vérification pas déjà publiée
         if($sortie->getEtat() === $etatRepo->findOneBy(['libelle' => 'Ouverte'])){
             $this->addFlash("error", "Sortie déjà publiée");
             //todo: changement de redirect vers la page détail de la sortie
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
 
         //Si rien n'est déclenché : on publie
@@ -193,10 +193,10 @@ final class ParticipationController extends AbstractController
         $manager->flush();
 
         $this->addFlash("success", "Sortie publiée");
-        return $this->redirectToRoute("app_home");
+        return $this->redirectToRoute("app_accueil");
     }
 
-    #[Route('/sorties/{id}/annulation', name: 'participation_annulation', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    #[Route('/sortie/{id}/annulation', name: 'participation_annulation', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
     public function annulationSortie(
         Sortie $sortie,
         EtatRepository $etatRepo,
@@ -218,7 +218,7 @@ final class ParticipationController extends AbstractController
         //bien l'organisateur de l'event
         if($participant !== $sortie->getOrganisateur()){
             $this->addFlash("error", "Vous n'êtes pas l'organisateur.");
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
         //todo: décommenter une fois la mise en place du token CSRF sur les pages twig
         /*
@@ -227,7 +227,7 @@ final class ParticipationController extends AbstractController
         if(!$this->isCsrfTokenValid('inscrire'.$sortie->getId(), (string) $request->request->get('_token')))
         {
             $this->addFlash("error", "Action non autorisée (CSRF).");
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
         */
 
@@ -237,20 +237,20 @@ final class ParticipationController extends AbstractController
         if($sortie->getDateHeureDebut() < $now){
             $this->addFlash("error", "Pas d'annulation d'événements déjà commencé.");
             //todo: changement de redirect vers la page détail de la sortie
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
         //Vérification pas déjà annulée
         if($sortie->getEtat() === $etatRepo->findOneBy(['libelle' => 'Annulée'])){
             $this->addFlash("error", "Sortie déjà annulée");
             //todo: changement de redirect vers la page détail de la sortie
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
 
         //Bien publiée
         if(!($sortie->getEtat() === $etatRepo->findOneBy(['libelle' => 'Ouverte']))){
             $this->addFlash("error", "Sortie non publiée");
             //todo: changement de redirect vers la page détail de la sortie
-            return $this->redirectToRoute("app_home");
+            return $this->redirectToRoute("app_accueil");
         }
 
         //Si rien n'est déclenché : on annule
@@ -260,6 +260,6 @@ final class ParticipationController extends AbstractController
         $manager->flush();
 
         $this->addFlash("success", "Sortie annulée");
-        return $this->redirectToRoute("app_home");
+        return $this->redirectToRoute("app_accueil");
     }
 }
