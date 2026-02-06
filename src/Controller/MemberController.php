4b878512c2839a07a23f609933e5c984a31571d7
Fabien Hoareau <fabienhoareau.dev@gmail.com>
2026-02-06 17:16:32 +0100

    feat: Add a member profile page to display participant details by pseudo.

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -5,12 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use App\Entity\Participant;
 
 final class MemberController extends AbstractController
 {
-    #[Route('/profil/{pseudo}', name: 'app_member_show', methods: ['GET'])]
-    public function show(Participant $participant): Response
+    #[Route('/profil/{pseudo}', 
+        name: 'app_member_show',
+        requirements: ['pseudo' => '^(?!modifier$).+'], 
+        methods: ['GET']
+    )]
+    public function show(
+        #[MapEntity(mapping: ['pseudo' => 'pseudo'])]
+        Participant $participant): Response
     {
         return $this->render('member/show.html.twig', [
             'participant' => $participant,
